6500717cb37b0f83e14e7edbfb642610aba29344
Santiago <santiagocobanera@gmail.com>
2020-12-26 15:11:56 -0300

    Creates SHA-256 Hash Calculator main file.

---
+++ SHA-256 Hash/SHA-256 Hash.cpp
@@ -0,0 +1,11 @@
+// SHA-256 Hash.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+    cout << "Input: ";
+}
