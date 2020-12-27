598ada94df8c282aa5d173b7e767b761a3417a82
Santiago <santiagocobanera@gmail.com>
2020-12-27 17:07:27 -0300

    Update main.

--- SHA-256 Hash/SHA-256 Hash.cpp
+++ SHA-256 Hash/SHA-256 Hash.cpp
@@ -1,11 +1,14 @@
 // SHA-256 Hash.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "SHA256.h"
 #include <iostream>
 
 using std::cout;
+using std::endl;
 
 int main()
 {
-    cout << "Input: ";
+    SHA256 hash_service;
+    cout << hash_service.hash("abc");
 }
